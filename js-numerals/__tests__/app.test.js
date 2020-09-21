9fb0f6d86f56f118bfa5d3de489ffab79c940340
Barnabas Bucsek <bucsekbarnabas@gmail.com>
2020-09-21 18:31:24 +0200

    clear up test file, add more tests

--- js-numerals/__tests__/app.test.js
+++ js-numerals/__tests__/app.test.js
@@ -1,21 +1,16 @@
 import convertNumber from '../app'
 
-test('whatever', () => {
-  document.body.innerHTML =
-    '<div>' +
-    '  <input class="input-number" />' +
-    '  <button class="btn" />' +
-    '</div>';
-
-  const num = document.querySelector('.input-number')
-  const btn = document.querySelector('.btn')
-  num.value = 10;
-  btn.addEventListener('click', function() {
-    convertNumber(num.value)
-  })
-
-  expect(convertNumber(10)).toBe("ten");
-  expect(convertNumber(11)).toBe("eleven");
-  expect(convertNumber(1000)).toBe("one thousand");
-
+test('10 to be equal ten', () => {
+  expect(convertNumber(10)).toBe("ten"); 
 });
+
+test('11 to be equal eleven', () => {
+  expect(convertNumber(11)).toBe("eleven"); 
+});
+
+test('1000 to be equal one thousand', () => {
+  expect(convertNumber(1000)).toBe("one thousand"); 
+});
+
+
+  
