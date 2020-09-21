5054f8ab56be90b543d7f152ab0b2111c509e718
Barnabas Bucsek <bucsekbarnabas@gmail.com>
2020-09-21 10:05:27 +0200

    add rule six method

--- js-numerals/app.js
+++ js-numerals/app.js
@@ -1,10 +1,7 @@
 // Ruleset: https://www.grammarbook.com/numbers/numbers.asp
-// Hyphenate all compound numbers from twenty-one through ninety-nine.
-// When writing out a number of three or more digits, the word and is not necessary.
-// The simplest way to express large numbers is usually best.
-
-const num = document.querySelector(".number");
-const btn = document.querySelector(".btn");
+// Rule 2a: Hyphenate all compound numbers from twenty-one through ninety-nine.
+// Rule 6: The simplest way to express large numbers is usually best.
+// Rule 8a: When writing out a number of three or more digits, the word and is not necessary.
 
 const digits = {
   0: "",
@@ -47,36 +44,54 @@ const aboveHundred = {
   4: "trillion",
 };
 
-btn.addEventListener("click", function () {
-  let number = num.value;
-  let writtenNumber = conversion(number);
-});
+function app() {
+  const num = document.querySelector(".number");
+  const btn = document.querySelector(".btn");
 
-function tens(num) {
-  if (num == 0) return "";
-  if (num < 20) return digits[num];
-  return doubleDigits[parseInt(num / 10)] + "-" + digits[num % 10];
-}
-
-function hundreds(num) {
-  if (num == 0) return "";
-  if (num < 100) return tens(num % 100);
-  return digits[parseInt(num / 100)] + " hundred " + tens(num % 100);
-}
-
-function conversion(num) {
-  if (num == 0) return "Zero";
-  let c = 0;
-  let wrd = "";
-  while (num > 0) {
-    h = num % 1000;
-    if (h != 0) {
-      wrd = `${hundreds(h)} ${aboveHundred[c]} ${wrd}`;
+  btn.addEventListener("click", function () {
+    let number = num.value;
+    let numberList = Array.from(number.toString()).map(Number);
+    if (number < 10000 && numberList[1] != 0) {
+      getRuleSixResult(numberList)
     }
-    num = parseInt(num / 1000);
-    c = c + 1;
+    let writtenNumber = conversion(number);
+
+  });
+
+  function getRuleSixResult(numberList) {
+    let firstTwo = `${numberList[0]}${numberList[1]}`
+    let lastTwo = `${numberList[2]}${numberList[3]}`
+    let writtenNumber = `${tens(firstTwo)} hundred ${tens(lastTwo)}`
+    console.log(writtenNumber)
   }
 
-  console.log(wrd);
-  return wrd;
+  function tens(num) {
+    if (num == 0) return "";
+    if (num < 20) return digits[num];
+    return doubleDigits[parseInt(num / 10)] + "-" + digits[num % 10];
+  }
+
+  function hundreds(num) {
+    if (num == 0) return "";
+    if (num < 100) return tens(num % 100);
+    return digits[parseInt(num / 100)] + " hundred " + tens(num % 100);
+  }
+
+  function conversion(num) {
+    if (num == 0) return "Zero";
+    let c = 0;
+    let wrd = "";
+    while (num > 0) {
+      h = num % 1000;
+      if (h != 0) {
+        wrd = `${hundreds(h)} ${aboveHundred[c]} ${wrd}`;
+      }
+      num = parseInt(num / 1000);
+      c = c + 1;
+    }
+    return wrd;
+  }
+
 }
+
+app();
