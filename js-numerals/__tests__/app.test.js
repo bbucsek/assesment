8509679f9270e348f7add3a3542d539de6219df7
Barnabas Bucsek <bucsekbarnabas@gmail.com>
2020-09-21 18:45:06 +0200

    add more test cases

--- js-numerals/__tests__/app.test.js
+++ js-numerals/__tests__/app.test.js
@@ -26,4 +26,23 @@ test('770243101 to be equal to: seven hundred seventy million two hundred forty-
   test('1200 to be equal to: twelve hundred', () => {
     expect(convertNumber(1200)).toBe("twelve hundred"); 
   });
+
+  test('5555 to be equal to: fifty-five hundred fifty-five', () => {
+    expect(convertNumber(5555)).toBe("fifty-five hundred fifty-five"); 
+  });
+
+  test('9901 to be equal to: ninety-nine hundred one', () => {
+    expect(convertNumber(9901)).toBe("ninety-nine hundred one"); 
+  });
+  
+
+  //test huge numbers
+
+  test('9748280719914710 to be equal to: nine quadrillion seven hundred forty-eight trillion two hundred eighty billion seven hundred nineteen million nine hundred fourteen thousand seven hundred ten', () => {
+    expect(convertNumber(9748280719914710)).toBe("nine quadrillion seven hundred forty-eight trillion two hundred eighty billion seven hundred nineteen million nine hundred fourteen thousand seven hundred ten"); 
+  });
+
+  test('1648264559103 to be equal to: one trillion six hundred forty-eight billion two hundred sixty-four million five hundred fifty-nine thousand one hundred three', () => {
+    expect(convertNumber(1648264559103)).toBe("one trillion six hundred forty-eight billion two hundred sixty-four million five hundred fifty-nine thousand one hundred three"); 
+  });
   
