5c241f087c8a59c88a90c667caa16f289ad442f2
Barnabas, Bucsek <bucsek.barnabas@gmail.com>
2020-09-29 18:14:34 +0200

    add react router. add material ui core. add basic structure.

--- js-users/react-js-users/src/App.js
+++ js-users/react-js-users/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Sidebar from "./Components/Sidebar";
+import List from "./Components/List";
 
 function App() {
   return (
-    <div className="App">
-      hello world
+    <div className="app">
+      <div className="app-body">
+        <Router>
+          <Sidebar />
+          <Route path="/">
+            <List />
+          </Route>
+        </Router>
+      </div>
     </div>
   );
 }
---
+++ js-users/react-js-users/src/Components/List.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Components/List.css'
+
+function List() {
+    return (
+        <div className="list">
+            hello
+        </div>
+    )
+}
+
+export default List
