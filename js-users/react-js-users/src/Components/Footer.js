967325e807e3e754b89d98a84e0389bb5a18a7a7
Barnabas, Bucsek <bucsek.barnabas@gmail.com>
2020-10-02 09:25:03 +0200

    add new files

---
+++ js-users/react-js-users/src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Components/Footer.css'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <h3>email@email.com</h3>
+            <h3>js-users-2020</h3>
+            <h3>all rights reserved</h3>
+        </div>
+    )
+}
+
+export default Footer
